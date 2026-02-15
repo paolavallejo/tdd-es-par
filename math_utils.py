e36fe9a95b3169450cf428a256a5b75ffe910400
paolavallejo <38500476+paolavallejo@users.noreply.github.com>
2026-02-15 19:35:35 +0000

    RDD

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,6 @@
 def es_par(n: int) -> bool:
-    return n % 2 == 0
+    """
+    Devuelve True si 'n' es un número par; en caso contrario, False.
+    Un entero es par cuando el residuo de dividirlo entre 2 es 0.
+    """
+    return (n % 2) == 0
